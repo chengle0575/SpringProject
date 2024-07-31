a069de2ea1346c1ffb9d9aabed8d79620081e5db
carolCheng <chengle0575@foxmail.com>
2024-07-31 16:24:32 +0800

    fix bug: Model refer to ui.Model brlonging to library

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
-
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller //stereotype annotation for adding bean to context
 public class MainController {
 
-    @RequestMapping("/") //associate below method to an HTTP request
-    public String home(){
+    @RequestMapping("/")//associate below method to an HTTP request
+    public String home(Model page){ //the Model refer to ui.model in package
+        page.addAttribute("username","Katy");
+        page.addAttribute("color","redhom");
         return "homepage.html"; //return the HTML name that we want tthe brower to display
     }
 }
