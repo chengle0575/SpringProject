6a3bdc047d4b1902367d2f7d823a47d0cd40a02b
carolCheng <chengle0575@foxmail.com>
2024-08-05 17:03:17 +0800

    HTTP request using request parameter

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -2,15 +2,20 @@ package com.example.demo;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+gitqq
 @Controller //stereotype annotation for adding bean to context
 public class MainController {
 
     @RequestMapping("/")//associate below method to an HTTP request
-    public String home(Model page){ //the Model refer to ui.model in package
-        page.addAttribute("username","Katy");
-        page.addAttribute("color","redhom");
+    public String home(Model page,@RequestParam String color, @RequestParam String name){ //the Model refer to ui.model in package
+                                    //parameter to retrieve from HTTP request parameter
+
+        page.addAttribute("username",name);
+        page.addAttribute("color",color);
+
+
         return "homepage.html"; //return the HTML name that we want tthe brower to display
     }
 }
