73d10eb0a15037efb313286efad9628660ee4883
carolCheng <chengle0575@foxmail.com>
2024-08-05 17:05:00 +0800

    HTTP request using path variable

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -1,18 +1,19 @@
 package com.example.demo;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-gitqq
+
 @Controller //stereotype annotation for adding bean to context
 public class MainController {
 
-    @RequestMapping("/")//associate below method to an HTTP request
-    public String home(Model page,@RequestParam String color, @RequestParam String name){ //the Model refer to ui.model in package
+    @RequestMapping("/{color}")//associate below method to an HTTP request
+    public String home(Model page,@PathVariable String color){ //the Model refer to ui.model in package
                                     //parameter to retrieve from HTTP request parameter
 
-        page.addAttribute("username",name);
+        page.addAttribute("username","Judy");
         page.addAttribute("color",color);
 
 
